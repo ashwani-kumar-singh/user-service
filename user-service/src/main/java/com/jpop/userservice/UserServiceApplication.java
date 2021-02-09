7fc7d601074b275049b1987338c1cda7cbd11a2f
Ashwani Kumar Singh <ashwanikumarsingh@outlook.com>
2021-02-09 11:19:17 +0530

    zuul gateway

--- user-service/src/main/java/com/jpop/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/jpop/userservice/UserServiceApplication.java
@@ -2,12 +2,12 @@ package com.jpop.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
